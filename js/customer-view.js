23353d266c70c06247b6635eadcfb7bb9b525fef
mahmoudalameh <mahmoudalameh56@gmail.com>
2026-09-17 04:37:53 +0300

    Update customer-view.js

--- js/customer-view.js
+++ js/customer-view.js
@@ -1,5 +1,6 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js".replace("firestore", "app");
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
 
 // ===== Firebase =====
 const firebaseConfig = {
